c9df6b050bb37d799806b586912dd3ecf9ca27d7
Vladislav <labkovich.vl@gmail.com>
2024-02-03 15:47:03 +0300

    feat: add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import {TopCars, topCars} from "./components/TopCars";
+import {Practice, students} from "./components/Practice";
 
 
 function App() {
     return (
-        // <Practice students1={students}/>
-        <TopCars topcars1={topCars}/>
+        <>
+            <Practice students1={students}/>
+            <TopCars topcars1={topCars}/>
+        </>
+
     );
 }
 
