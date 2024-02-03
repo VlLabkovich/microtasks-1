815e13621dfee9c1f525de9745fab0242ed768f2
Vladislav <labkovich.vl@gmail.com>
2024-02-03 15:30:19 +0300

    feat: add props and method map

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {TopCars, topCars} from "./components/TopCars";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        // <Practice students1={students}/>
+        <TopCars topcars1={topCars}/>
+    );
 }
 
 export default App;
