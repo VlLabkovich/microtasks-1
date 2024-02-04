9c2974351b6ee0e3a5d5eebc58c8e036e6a0c914
Vladislav <labkovich.vl@gmail.com>
2024-02-04 17:41:35 +0300

    refactor: add examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, {MouseEvent} from "react";
 import './App.css';
 
 
 function App() {
-    const myFirstSubscriber = () => {
-        alert('Hello Im Vasya')
+
+    // const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+    //     alert('Hello Im Vasya')
+    // }
+    //
+    // const mySecondCubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+    //     alert('Hello Im Ivan')
+    // }
+
+    // const onClickHandler = (name: string) => {
+    //     console.log(name)
+    // }
+
+    const foo1 = () => {
+        console.log(100200)
     }
 
-    const mySecondCubscriber = () => {
-        alert('Hello Im Ivan')
+    const foo2 = (num: number) => {
+        console.log(num)
     }
+
     return (
         <div className="App">
-            <button onClick={myFirstSubscriber}>MyYoutubeChannel</button>
-            <button onClick={mySecondCubscriber}>MyYoutubeChannel2</button>
+            {/*<button onClick={(event) => {console.log("Hello")}}>MyYoutubeChannel-1</button>*/}
+            {/*<button onClick={myFirstSubscriber}>MyYoutubeChannel-1></button>*/}
+
+            {/*<button onClick={(event: MouseEvent<HTMLButtonElement> ) => onClickHandler("VASYA")}>MyYoutubeChannel-l</button>*/}
+            {/*<button onClick={(event: MouseEvent<HTMLButtonElement> ) => onClickHandler("IVAN")}>MyYoutubeChannel-2</button>*/}
+            {/*<button onClick={() => onClickHandler("some info")}>MyYoutubeChannel-3</button>*/}
+
+            <button onClick={foo1}>1</button>
+            <button onClick={() => foo2(100200)}>2</button>
         </div>
     );
 }
