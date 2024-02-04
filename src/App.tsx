2163ef8675e9322a3f0b886072e28e89091aa4e2
Vladislav <labkovich.vl@gmail.com>
2024-02-04 18:56:25 +0300

    refactor: add button3

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,23 @@ import {Button} from "./components/Button";
 
 function App() {
 
-    const ButtonFoo1 = () => {
-        console.log("Im Vasya")
+    const Button1 = (subscriber: string, age: number) => {
+        console.log(subscriber,' - ',age)
     }
 
-    const ButtonFoo2 = () => {
-        console.log("Im Ivan")
+    const Button2 = (subscriber: string, age: number) => {
+        console.log(subscriber,' - ',age)
+    }
+
+    const Button3 = () => {
+        console.log("Im stupid button")
     }
 
     return (
         <div className="App">
-            <Button name={'MyYouTubeChannel-1'} callBack={ButtonFoo1}/>
-            <Button name={'MyYouTubeChannel-2'} callBack={ButtonFoo2}/>
+            <Button name={'MyYouTubeChannel-1'} callBack={() => Button1("Im Vasya", 25)}/>
+            <Button name={'MyYouTubeChannel-2'} callBack={() => Button2("Im Ivan", 30)}/>
+            <Button name={'Stupid button'} callBack={Button3}/>
         </div>
     );
 }
