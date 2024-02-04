afd48d6834f56498fb12f8aedff3efc0c37487ab
Vladislav <labkovich.vl@gmail.com>
2024-02-04 22:40:23 +0300

    feat: add UseState

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 
 function App() {
+
+    // let a = 1
+
+    let [a, setA] = useState(1)
+
+    const onClickHandlerOne = () => {
+        setA(++a)
+    }
+
+    const onClickHandlerTwo = () => {
+        setA(0)
+    }
+
     return (
-        <div className="root">
-            <h1>Hello World</h1>
+        <div className="App">
+            <h1>{a}</h1>
+            <button onClick={onClickHandlerOne}>number</button>
+            <button onClick={onClickHandlerTwo}>0</button>
         </div>
-    );
+    )
 }
 
 export default App;
