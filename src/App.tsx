022b11f6d2df2cb7bf87a3a190553868efc827ee
Vladislav <labkovich.vl@gmail.com>
2024-02-05 13:21:55 +0300

    feat: add UseState

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 
 function App() {
+
+    const [money, setMoney] = useState([
+        {banknote: 'dollar', nominal: 100, number: 'a1234567890'},
+        {banknote: 'dollar', nominal: 50, number: 'z1234567890'},
+        {banknote: 'ruble', nominal: 100, number: 'w1234567890'},
+        {banknote: 'dollar', nominal: 100, number: 'e1234567890'},
+        {banknote: 'dollar', nominal: 50, number: 'c1234567890'},
+        {banknote: 'ruble', nominal: 100, number: 'r1234567890'},
+        {banknote: 'dollar', nominal: 50, number: 'x1234567890'},
+        {banknote: 'ruble', nominal: 50, number: 'v1234567890'},
+    ])
+
+
     return (
-        <div className="root">
-            <h1>Hello World</h1>
-        </div>
+        <ul>
+            {money.map((objFromMoneyArr) => {
+                return (
+                    <li>
+                        <span>{objFromMoneyArr.banknote}</span>
+                        <span>{objFromMoneyArr.nominal}</span>
+                        <span>{objFromMoneyArr.number}</span>
+                    </li>
+                )
+            })}
+        </ul>
     );
 }
 
